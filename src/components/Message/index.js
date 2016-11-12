bfb61a8803fc32e6c41a466efde2078437c8c119
Kamalakannan <kamalaknn@users.noreply.github.com>
2016-11-12 18:54:54 +0530

    Display message time (#5)

--- src/components/Message/index.js
+++ src/components/Message/index.js
@@ -1,5 +1,13 @@
 import React, { Component } from 'react';
+import moment from 'moment';
+
 import './style.css';
+
+moment.locale('en-IN', {
+  calendar: {
+    sameDay: 'LT'
+  }
+});
 
 export default class extends Component {
   render() {
@@ -7,10 +15,16 @@ export default class extends Component {
     let classNames = 'message-wrapper';
     let sentClass = message.isSent ? 'message-wrapper__sent' : 'message-wrapper__received';
     classNames = classNames + ' ' + sentClass;
+    let timeString = message.isSending ? 'Sending...' : moment(message.time).calendar();
     return (
       <div className={classNames}>
         <span className='message'>
-          {message.contents} {message.isSending && 'Sending...'}
+          <span>
+            {message.contents}
+          </span>
+          <span className='message--timestamp'>
+             {timeString}
+          </span>
         </span>
       </div>
     );
